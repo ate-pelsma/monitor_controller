ce64849f908e1caa4dde7d1cb0390aa706548517
Ate Pelsma <87018841+ate-pelsma@users.noreply.github.com>
2024-11-08 14:34:54 +0100

    first commit

---
+++ __main__.py
@@ -0,0 +1,42 @@
+import time
+import threading
+
+from logger import setup_logger
+from devicelistener import DeviceListener
+from eventcontroller import EventController
+from config import ConfigGenerator
+
+def main():
+    logger = setup_logger()
+    logger.info("Program starting...")
+
+    config = ConfigGenerator().execute()
+
+    event_queue = []
+
+    print("The program will now start listening for events.")
+
+    thread1 = threading.Thread(target=execute, args=(logger, config, event_queue))
+    thread1.daemon = True 
+    thread1.start()
+
+    try:
+        input("Press enter to exit program ...")
+    except KeyboardInterrupt:
+        pass
+
+def execute(logger, config, event_queue):
+    while True:
+        listener = DeviceListener(config["trigger_device"], event_queue)
+        listener.listen()
+
+        controller = EventController(event_queue, config["settings"])
+        controller.handle_event()
+
+        logger.info("4 second time out to prevent too early signal detection.")
+        time.sleep(4)
+        logger.info("Program is done sleeping, starting from the top...")
+
+if __name__ == "__main__":
+
+    main()
